e53b34e1c2c8ef8f5e4b1c010476c558ed257cf1
404-lorena <lramirezlopez@google.com>
2023-03-28 10:07:15 -0400

    Refactoring

--- src/com/dalelore/Main.java
+++ src/com/dalelore/Main.java
@@ -11,21 +11,40 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
         // User input
         Scanner scanner = new Scanner(System.in);
 
         // Prompts
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        while(true){
+            System.out.print("Principal ($1K - $1M): ");
+            principal = scanner.nextInt();
+            if(principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a number between 1,000 and 1,000,000");
+        }
 
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        while(true){
+            System.out.print("Annual Interest Rate: ");
+            float annualInterest = scanner.nextFloat();
+            if(annualInterest>= 1 && annualInterest <= 30){
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
-        System.out.print("Period (in years): ");
-        byte years = scanner.nextByte();
-        int numerOfPayments = years * MONTHS_IN_YEAR;
-
+        while(true) {
+            System.out.print("Period (in years): ");
+            byte years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+        }
         // Calculating mortage payment formula
         /**     M = P [ i(1 + i)^n ] / [ (1 + i)^n – 1].
                 M = Total monthly payment.
@@ -34,7 +53,7 @@ public class Main {
                 N = The total amount of months in your timeline for paying off your mortgage.
          **/
 
-        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numerOfPayments) / (Math.pow(1 + monthlyInterest, numerOfPayments)-1));
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments) / (Math.pow(1 + monthlyInterest, numberOfPayments)-1));
 
         String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
 
